fe5de412767e59d3bb36549ed38d8a757ecc53d1
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-03 04:44:45 +0000

    este programa  te pedirá tu nombre y tu edad

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # repaso
 crear 26 programas como mínimo 
+
+## indice
+|No.|Archivo|Descripcion|
+|--|--|--|
+|1|programa1.py|Hola mundo en python3|
---
+++ repaso/programa1.py
@@ -0,0 +1,11 @@
+##suma de datos 
+print()
+mensaje = "Hola querido usuario a continuacion se te pedira tu nombre y tu edad "
+print (mensaje)
+print()
+nombre = input("Ingrese su nombre: ")
+#aqui va a leer el nombre y arrojara el texto con su nombre
+edad = input("Ingrese su edad: ")
+print()
+print("Tu nombre es "+nombre, "y tienes una edad de "  +edad, "años")
+print()
