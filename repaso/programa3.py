8dcca5cd8eaf3b80dfe695c8caa7baebd611b8ad
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-05 03:11:06 +0000

    En este programa de acuerdo a tú edad se te informa si eres  menor o mayor de edad

---
+++ repaso/programa3.py
@@ -0,0 +1,8 @@
+#en este programa ingresaras tú edad y te dirá si eres mayor o menor
+
+edad = int(input("¿Cuál es tú edad?"))
+if edad < 18:
+    print("Eres menor de edad ")
+else: 
+    print("Eres mayor de edad ")
+    
