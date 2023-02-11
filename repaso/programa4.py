0ddc4d29cbf49d548c51e19dead4aa42772c7260
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-11 02:42:12 +0000

    Se utiliza condicionantes para impresión de mensajes

---
+++ repaso/programa4.py
@@ -0,0 +1,12 @@
+#este programa se utiliza condición solo para que arrojé si es verdad arroja información y si no, no arrojará nada 
+x=5
+if x > 5:
+    print("Hola mundo")
+
+
+##Esta condición si te arroja un mensaje de salida
+
+color = "Negro"
+
+if color == "Negro":
+    print("Mi color favorito es este ")
